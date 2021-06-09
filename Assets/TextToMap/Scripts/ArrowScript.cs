0fbbda6965268df6c4238379611671f2fe6941e3
ovilapu <oscar08850@gmail.com>
2021-06-09 23:06:07 +0200

    La flecha se destruye
    La flecha se destruye

---
+++ Assets/TextToMap/Scripts/ArrowScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour
+{
+    GameObject objeto;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        Destroy(gameObject, 2f);
+    }
+}
