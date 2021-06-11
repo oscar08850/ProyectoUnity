03d1bfa66e32940bf0d265becc5bd2661b6496da
Àlex Moya Sánchez <62418737+alexmoyasanchez@users.noreply.github.com>
2021-06-11 08:38:55 +0200

    Cámara sigue a personaje
    cámara sigue a personaje

---
+++ Assets/TextToMap/Scripts/Camara.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    public Transform player;
+
+    Vector3 vel = Vector3.zero;
+
+    public float suavidadCamara = .15f;
+
+    void FixedUpdate()
+    {
+        Vector3 playerpos = player.position;
+        playerpos.z = transform.position.z;
+
+        transform.position = Vector3.SmoothDamp(transform.position, playerpos, ref vel, suavidadCamara);
+    }
+}
