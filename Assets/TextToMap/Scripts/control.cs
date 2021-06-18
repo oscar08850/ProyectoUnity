451a1f6d4992be09a31d59e425223d208e9a2d32
ovilapu <oscar08850@gmail.com>
2021-06-18 23:56:04 +0200

    Joystick funciona mapa inicial
    joystick funciona mapa inicial

--- Assets/TextToMap/Scripts/control.cs
+++ Assets/TextToMap/Scripts/control.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using EasyJoystick;
 
 public class control : MonoBehaviour
 {
 
     public Animator animator;
+
+    [SerializeField] public float speed;
+    [SerializeField] public Joystick joystick;
+    private float movX, movY;
+
 
     public Vector2 movement;
     Vector2 mousePos;
@@ -44,6 +50,7 @@ public class control : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        this.joystick = FindObjectOfType<Joystick>();
 
         transform.rotation = Quaternion.identity;
         currentHealth = GameObject.FindGameObjectWithTag("EstadoJuego").GetComponent<EstadoJuego>().GetHealth();
@@ -69,7 +76,7 @@ public class control : MonoBehaviour
 
 
 
-    
+
 
 
     private void FixedUpdate()
@@ -86,6 +93,12 @@ public class control : MonoBehaviour
 
     void movements()
     {
+
+
+
+
+#if UNITY_STANDALONE || UNITY_WEBPLAYER
+
         float inputMovimiento = Input.GetAxis("Horizontal");
         movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
@@ -126,9 +139,55 @@ public class control : MonoBehaviour
         else
             animator.SetBool("RunPlayer", false); //IDLE STATE
 
+#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
 
 
+        movX = joystick.Horizontal();
+        movY = joystick.Vertical();
+        movement = new Vector2(movX,movY);
+
+        if (movement != Vector2.zero)
+        {
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+            animator.SetFloat("Magnitude", movement.magnitude);
+
+            if (movement.x < 0) //Pulso IZQUIERDA
+            {
+                if (mirandoDerecha == true) //VENGO DE MIRAR DERECHA
+                {
+                    transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+                    mirandoDerecha = false;
+                }
+                else //VENGO DE MIRAR IZQUIERDA
+                {
+                    mirandoDerecha = false;
+                }
+
+            }
+            else if (movement.x > 0) //Pulso DERECHA
+            {
+                if (mirandoDerecha == true) //VENGO DE MIRAR DERECHA
+                {
+                    mirandoDerecha = true;
+                }
+                else //VENGO DE MIRAR IZQUIERDA
+                {
+                    transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+                    mirandoDerecha = true;
+                }
+            }
+
+            animator.SetBool("RunPlayer", true); //MOVEMENT STATE
+        }
+        else
+            animator.SetBool("RunPlayer", false); //IDLE STATE
+
+       // transform.position += new Vector3(movX, movY, 0f) * speed * Time.deltaTime;
+
+#endif
         // transform.position = transform.position + movement * Time.deltaTime * velocidad;
+
     }
 
     public void TakeDamage(int damage)
@@ -140,7 +199,8 @@ public class control : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Coin") {
+        if (collision.tag == "Coin")
+        {
             GameObject.FindGameObjectWithTag("EstadoJuego").GetComponent<EstadoJuego>().AddMonedas(1);
             GameObject.FindGameObjectWithTag("Coin").GetComponent<ScoreManager>().PintarScore();
         }
